5383957682dcfdafc560345acc897e391df56a61
Nikhil Saini <nikhil.nextbyts@gmail.com>
2024-12-23 19:35:10 +0530

    resolve the city input problam and degine responsivness

--- weather.js
+++ weather.js
@@ -1,10 +1,8 @@
+//accesing the api key
 let baseURL = "https://api.openweathermap.org/data/2.5/weather?q=";
 let apiKey = "&units=metric&appid=75e6434cb45ef6acfeaee7f49dbcfc03";
 
-let city = "agra";
-
-const city_name = document.querySelector("#city").value;
-
+//accesing the dom elements
 const btn = document.querySelector("#checkWeather");
 const tmp = document.querySelector("#tmp");
 const feels = document.querySelector("#feel");
@@ -12,25 +10,28 @@ const w_speed = document.querySelector("#w_speed");
 const hmd = document.querySelector("#hmd");
 const wdc = document.querySelector("#wdc");
 
-const fetch_weather = async () => {
-     let URL = `${baseURL}${city_name}${apiKey}`; 
+//fetching the weather data
+const fetch_weather = async (city) => {
+    let URL = baseURL+city+apiKey; 
     let response = await fetch(URL);
     let data = await response.json();
     if(response.ok) {
-        let tempreture = data.main.temp;
-        let feels_like = data.main.feels_like;
+        let tempreture = Math.round(data.main.temp);
+        let feels_like = Math.round(data.main.feels_like);
         let Humidity = data.main.humidity;
         let weather_condition = data.weather[0].description;
-        let wind_speed = data.wind.speed;
+        let wind_speed = Math.round(data.wind.speed);
 
         tmp.innerText = tempreture;
         feels.innerText = feels_like;
         w_speed.innerText = wind_speed;
         hmd.innerText = Humidity;
         wdc.innerText = weather_condition;
-    }
+    } 
 }
 
+//event listener for the button
 btn.addEventListener("click", () => {
-    fetch_weather();
+  const city_name = document.querySelector("#city").value;
+  fetch_weather(city_name);
 });
